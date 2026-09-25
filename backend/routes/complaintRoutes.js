fcd5474a6d2b4757740068e62945b2c71978da9d
Sai sahasra meghana Kakarlapudi <sahasra1428@gmail.com>
2026-09-25 20:48:56 +0530

    Refactor complaint routes for cleaner structure

--- backend/routes/complaintRoutes.js
+++ backend/routes/complaintRoutes.js
@@ -12,35 +12,30 @@ const {
     deleteComplaint
 } = require("../complaintController");
 
-// CREATE COMPLAINT
 router.post(
     "/",
     authMiddleware,
     createComplaint
 );
 
-// GET USER COMPLAINTS
 router.get(
     "/",
     authMiddleware,
     getComplaints
 );
 
-// GET SINGLE COMPLAINT
 router.get(
     "/:id",
     authMiddleware,
     getComplaintById
 );
 
-// UPDATE COMPLAINT
 router.put(
     "/:id",
     authMiddleware,
     updateComplaint
 );
 
-// DELETE COMPLAINT
 router.delete(
     "/:id",
     authMiddleware,
